c212ede19158d0f92e380a7febdecf5a8a3a6a49
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-09-19 21:39:41 +0200

    some progress on UI framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'budgeting-app';
+  active = 1;
+
+  months: string[] = [
+    '25 April - 25 May',
+    '25 June - 25 July',
+    '25 August - 25 September',
+  ];
 }
