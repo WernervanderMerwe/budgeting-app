5ee8eaef272f261ea3a26c9366f5659d81cba636
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-10-09 18:27:05 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { throwIfEmpty } from 'rxjs';
 import data from './data.json';
+
+// interface Entries {
+//   budget: string;
+//   description: string;
+//   amount: number;
+// }
 
 @Component({
   selector: 'app-root',
@@ -22,14 +29,75 @@ export class AppComponent implements OnInit {
     amount: number;
   }[];
 
+  budgetEntries: {
+    budget: string;
+    description: string;
+    amount: number;
+  }[];
+
+  groceriesFiltered: {
+    budget: string;
+    description: string;
+    amount: number;
+  }[] = [];
+  fuelFiltered: {
+    budget: string;
+    description: string;
+    amount: number;
+  }[] = [];
+  eatingOutFiltered: {
+    budget: string;
+    description: string;
+    amount: number;
+  }[] = [];
+  hobbiesFiltered: {
+    budget: string;
+    description: string;
+    amount: number;
+  }[] = [];
+
   constructor() {
     this.months = data.months;
     this.payments = data.monthlyPayments;
     this.budgets = data.monthlyBudgets;
+    this.budgetEntries = data.budgetEntries;
   }
 
   ngOnInit() {
-    // console.log(this.payments);
+    this.groceryFilter();
+    this.fuelFilter();
+    this.eatingOutFilter();
+    this.hobbiesFilter();
+    console.log(this.groceriesFiltered);
+  }
+
+  groceryFilter() {
+    const newArray = this.budgetEntries.filter(
+      (item) => 'Groceries' === item.budget
+    );
+    this.groceriesFiltered = [...newArray];
+    console.log(this.groceriesFiltered);
+  }
+
+  fuelFilter() {
+    const newArray = this.budgetEntries.filter(
+      (item) => 'Fuel' === item.budget
+    );
+    this.fuelFiltered = [...newArray];
+  }
+
+  eatingOutFilter() {
+    const newArray = this.budgetEntries.filter(
+      (item) => 'Eating Out' === item.budget
+    );
+    this.fuelFiltered = [...newArray];
+  }
+
+  hobbiesFilter() {
+    const newArray = this.budgetEntries.filter(
+      (item) => 'Hobbies' === item.budget
+    );
+    this.hobbiesFiltered = [...newArray];
   }
 
   get paymentSubTotal(): number {
@@ -47,4 +115,45 @@ export class AppComponent implements OnInit {
     this.budgets.forEach((val) => (value += val.amount));
     return value;
   }
+
+  get groceriesTotal(): number {
+    let value = 0;
+    this.groceriesFiltered.forEach((val) => (value += val.amount));
+    return value;
+  }
+
+  /*
+  someFn() {
+    
+  const someField = "Groceries";
+  
+  
+  //const items = budgetEntries.filter(i => i.budget === someField)
+  const budgetKeys = new Set(Object.keys(bodygetEnties));
+  const objInit = Object.entries(budgetKeys.values().map(i => ([i, []])))
+  
+  const budgetObject = budgetKeys.reduce((acc, curr) => {
+    acc[curr.budget].push(curr);
+  },objInit)
+  
+  
+  budgetObject.Groceries = [
+    { "budget": "Groceries", "description": "Checkers", "amount": 340 },
+    { "budget": "Groceries", "description": "Woolies", "amount": 420 },
+  ]
+  
+  
+  ////
+  let someActiveDataSet;
+  function onClick(someKey) {
+    return function() {
+      someActiveDataSet = budgetObject[someKey]
+    }
+  }
+  
+  
+  ////
+  ngFor="let budgetItems of someActiveDataSet"
+  }
+  */
 }
